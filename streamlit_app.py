bc7820b16636d415cc19a5f04189114753ec66cf
komal211203-kpk <komal211203@gmail.com>
2026-04-30 16:16:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Title
@@ -12,7 +11,8 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
 # Snowflake session
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Fetch fruit data - this matches the 'my_dataframe' used in the image
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
